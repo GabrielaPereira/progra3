70fc8dd65f098d0b43dc9de409af29f4133d2e83
GabrielaPereira <31809158+GabrielaPereira@users.noreply.github.com>
2017-09-11 15:49:47 -0300

    Add files via upload

--- TP1/src/Logica/Jugar.java
+++ TP1/src/Logica/Jugar.java
@@ -1,5 +1,6 @@
 package Logica;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JTextField;
@@ -8,22 +9,25 @@ public class Jugar {
 
 	private String[] operadores;
 	private int resultado;
-	private int numeros;
-	
+	private ArrayList numeros;
+	private ArrayList array_operadores;
+		
 	public Jugar (){
 		operadores = new String[4]; 
 		operadores[0] = "+";
 		operadores[1] = "-";
 		operadores[2] = "*";
 		operadores[3] = "/";
-		
+		numeros = new ArrayList<>();
 		resultado = 0;
+		array_operadores = new ArrayList();
 	}
 	
 	public String setOperador(){
 		Random aleatorio = new Random(System.currentTimeMillis());
 		int pos  = aleatorio.nextInt(4);
 		return operadores[pos];
+		
 	}
 	
 	public int buscarOperador(String s){
@@ -38,9 +42,24 @@ public class Jugar {
 	}
 	
 	public boolean verificar(){
+		int total = 0;
+		for(int a=0;a<numeros.size()-1;a++){
+			
+		}
 		
 		return true;
 	}
+	
+	public void guardarnumeros(int a){
+		numeros.add(a);
+	}
+	
+	public void guardaroperador(){
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Jugar n = new Jugar();
